8ac069c33c63b644124d2336fd966fd86776c502
Mukhammadazizkhon Jabborkhonov <mjabborkhonov@exadel.com>
2022-12-18 00:41:17 +0500

    Some changes

--- src/main/java/com/azikosolutions/springbootsns/controller/NotificationController.java
+++ src/main/java/com/azikosolutions/springbootsns/controller/NotificationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NotificationController {
+
     @Value("${topicARN}")
     private String TOPIC_ARN;
 
